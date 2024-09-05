106c11a0db2c8ba0cb8524ebe4fb8b6c6a9c5cd3
praveena <praveena@gmail.com>
2024-09-05 10:41:11 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+	
+	@GetMapping("/welcome")
+	public String welcomeMethod() {
+		return "Welcome!!!";
+	}
+	
+	@GetMapping("/greeting")
+	public String greetingMethod() {
+		return "greeting method";
+	}
+	@GetMapping("/hello")
+	public String helloMethod() {
+		return "hello";
+	}
+	
+
+}
